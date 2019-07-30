1a6d5ea008f41f83ef3ecd03024448f06cdc9f38
Udayudi <udaygam@gmail.com>
2019-07-30 14:04:44 +0530

    Server.js and routes

---
+++ routes/api/profile.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+// @route  GET api/profile/test
+// @desc   Tests post route
+// @access Public
+
+router.get("/test", (req, res) =>
+  res.json({
+    msg: "Profile Works"
+  })
+);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
 const app = express();
 
 // DB Config
@@ -11,6 +15,11 @@ mongoose
   .connect(db)
   .then(() => console.log("Database Connected"))
   .catch(err => console.log(err));
+
+// Use Routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
