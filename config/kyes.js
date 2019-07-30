b40e0a73c9b0ae7d5440126f4e5e603935f02fe1
Udayudi <udaygam@gmail.com>
2019-07-30 13:49:39 +0530

    Database Connection Added

---
+++ config/kyes.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://uday123:uday123@devconnector-1sq6c.mongodb.net/test?retryWrites=true&w=majority"
+};
--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
+
+// DB Config
+const db = require("./config/kyes").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("Database Connected"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
